fac0bf953fda0f1f42cf87223df61b1666df3824
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2024-11-14 18:36:33 +0000

    feat: install jest-environment-jsdom as a separate feature and test buttonClick() function

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -1,3 +1,19 @@
 /**
  * @jest-environment jsdom
  */
+
+const buttonClick = require("../button");
+
+beforeEach(() => {
+    // now identify the element we want to test
+    document.body.innerHTML = "<p id='par'></p>";
+});
+
+// now create the test
+describe("DOM tests", () => {
+    test("expects p content to change", () => {
+        // call the buttonClick() to click automatically
+        buttonClick()
+        expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+    });
+});
